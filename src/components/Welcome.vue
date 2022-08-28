d9957dc06e2022e1ed7d3a3b60a764dc60f2e149
Abdou Yaya Sadiakhou <saabdouyaya@ept.sn>
2022-08-28 22:34:53 +0000

    voting app DONE

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -46,17 +46,6 @@
         />
       </v-col>
     </v-row>
-    <v-container>
-      <div style="position: relative; padding-bottom: 56.25%; height: 0">
-        <iframe
-          src="https://www.loom.com/embed/7f964cabf846445091ac2c775b90f8b2"
-          frameborder="0"
-          webkitallowfullscreen
-          mozallowfullscreen
-          allowfullscreen
-          style="position: absolute; top: 0; left: 0; width: 100%; height: 100%"
-        ></iframe>
-      </div>
-    </v-container>
+   
   </section>
 </template>
